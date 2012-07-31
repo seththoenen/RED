0f20b4ac19a5b642c33bb37a0f175511a5796dfb
Seth Thoenen <seth_thoenen@hotmail.com>
2012-07-31 11:11:08 -0500

    Added InventoryDA

---
+++ SeniorProjectClassLibrary/SeniorProjectClassLibrary/DAL/InventoryDA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace SeniorProject
+{
+    class InventoryDA
+    {
+
+    }
+}
