d26200cac251a8eaab4fa30baafb0f1e25347421
Seth Thoenen <seth_thoenen@hotmail.com>
2012-08-01 14:55:50 -0500

    Created patch notes area for next release.

--- Red/SeniorProject/PatchNotes.aspx.designer.cs
+++ Red/SeniorProject/PatchNotes.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace SeniorProject {
         protected global::AjaxControlToolkit.Accordion Accordion1;
         
         /// <summary>
+        /// AccordionPaneNext control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.AccordionPane AccordionPaneNext;
+        
+        /// <summary>
         /// AccordionPane113 control.
         /// </summary>
         /// <remarks>
